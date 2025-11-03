bf83019accdea75c05748b5aff596091a428a73c
Àlex Milian <alexmiliandieguez@gmail.com>
2025-11-03 15:36:06 +0100

    Changes on lista_anime: read from matriz_corr.json

--- backend/app/routes/lista_anime.py
+++ backend/app/routes/lista_anime.py
@@ -1,9 +1,9 @@
 import os
+import json
 from flask import Flask, jsonify
-import pandas as pd
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-ANIME_CSV_PATH = os.path.join(BASE_DIR, "../data_base/data/anime.csv")
+ANIME_JSON_PATH = os.path.join(BASE_DIR, "../data_base/model/matriz_corr.json")
 
 class ListaAnime:
     def __init__(self, app: Flask):
@@ -14,9 +14,13 @@ class ListaAnime:
         @self.app.route("/lista_anime", methods=["GET"])
         def lista_anime():
             try:
-                anime_df = pd.read_csv(ANIME_CSV_PATH, usecols=[1], names=["name"], header=0)
-                anime_list = anime_df["name"].tolist()
+                with open(ANIME_JSON_PATH, "r", encoding="utf-8") as f:
+                    matriz_corr = json.load(f)
+                anime_list = list(matriz_corr.keys())
                 return jsonify(anime_list)
+
+            except FileNotFoundError:
+                return jsonify({"error": f"No se ha encontrado {ANIME_JSON_PATH}"}), 404
             except Exception as e:
                 return jsonify({"error": str(e)}), 500
 
